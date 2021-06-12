938d1ebf8457f125a228b645ef7a107ab947253b
Mick <mickx.shular@gmail.com>
2021-06-11 21:34:31 -0400

    Holy cow running and Health bar

---
+++ HolyCowGame/Assets/Scripts/HolyCowCollision.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolyCowCollision : MonoBehaviour
+{
+    private Rigidbody2D body;
+    [SerializeField] float forceAmount = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "DemonCow")
+        {
+            this.GetComponent<HolyCowHealthCount>().TakeDamage(20);
+
+            Vector2 temp = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
+            temp = temp.normalized;
+            body.AddForce(temp * forceAmount, ForceMode2D.Impulse);
+
+
+        }
+        if(collision.gameObject.tag == "SafeZone")
+        {
+            //add ui update
+            Debug.Log("One Point");
+        }
+    }
+
+}
